b186b94eb7a4500f6ad1cecafd7a83058805adf8
kem1101231 <k.nicor1101@gmail.com>
2018-08-13 11:35:51 +0800

    Test app for request

--- mgc_request_form_dev_1/__manifest__.py
+++ mgc_request_form_dev_1/__manifest__.py
@@ -26,7 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        #'views/display.xml',
+        'views/display.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
