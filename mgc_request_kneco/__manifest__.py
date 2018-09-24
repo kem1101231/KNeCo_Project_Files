5c6bc9abb64a359ccf6ef1f1352f6597fa973cb5
odoo <kris.condimina@gmail.com>
2018-09-24 04:05:10 -0400

    Update

--- mgc_request_kneco/__manifest__.py
+++ mgc_request_kneco/__manifest__.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mgc_request_kneco",
+    'name': "MGC Project Request",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        """,
 
     'description': """
-        Long description of module's purpose
+        Request Alternative
     """,
 
-    'author': "My Company",
+    'author': "MIS",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
@@ -27,9 +26,16 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/index.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'images': [
+        'static/description/icon.png',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
 }
--- mgc_request_kneco/models/request.py
+++ mgc_request_kneco/models/request.py
@@ -5,16 +5,38 @@ from odoo.addons import decimal_precision as dp
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 class MGC_Request(models.Model):
-	_name = "mgc.requeust.form.base"
+	_name = "mgc.request.form.base"
 	desciption = "MGC Request Forms"
 
-	name = None
-	requestor_id = None
-	request_department = None
-	request_company = None
+	name = fields.Char(string="Request Number", required=True)
+	requestor_id = fields.Many2one(comodel_name="hr.employee", string="Requesting Employee", required=True,  default=lambda self: self.env['hr.employee'].search([('user_id', '=', self.env.user.id)], limit=1))
+	request_department = fields.Many2one(comodel_name="hr.department", string="Department", required=True)
+	request_company = fields.Many2one(comodel_name="res.company", string="Company", required=True)
 	request_type = None
 	request_name = None
-	request_purpose = None
+	request_purpose = fields.Text(string="Purpose", required=True)
+	request_special_instruction = fields.Text(string="Special Instruction")
+
+	request_line_ids = fields.One2many("mgc.request.form.line", 'request_id', string="Request Name", required=False)
+
+	@api.onchange('requestor_id')
+	def _onchange_requestor_id(self):
+		if self.requestor_id:
+			self.request_company = self.requestor_id.company_id.id
+			self.request_department = self.requestor_id.department_id.id
+  
+
+class MGC_Request_Line(models.Model):
+	_name = 'mgc.request.form.line'
+
+	request_id = fields.Many2one(comodel_name="mgc.request.form.base", string="Request ID")
+	product_id = fields.Many2one(comodel_name="product.product", string="Product ID")
+	description = fields.Char(string="Description")
+	unit = fields.Char(string="Unit")
+	unit_cost = fields.Float(string="Unit Cost")
+	quantity = fields.Float(string="Quantity")
+	tax_rate = fields.Char(string="Tax")
+	subtotal = fields.Float(string="Sub Total")
 
 class MGC_Request_Types(models.Model):
 	_name = "mgc.requeust.form.types"
