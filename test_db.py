da05e5b6cc8012989ba3dccc4c0a3d1f8fbbc4ca
Kristopher Condimina <36105857+kem1101231@users.noreply.github.com>
2020-01-02 14:00:27 +0800

    Add files via upload

---
+++ test_db.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+
+if __name__ == '__main__':
+	
+	conn = psycopg2.connect(dbname='BRDC_UPDATE',user='postgres',host='localhost',port=5433, password='heathcliff')
+	print("+++++++++++++")
+	print(conn)
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM pg_catalog.pg_tables;")
+	conn.commit()
+	dat_out = cur.fetchall()
+
+	for line in dat_out:
+
+		print("____")
+		print(line)
+
+		
